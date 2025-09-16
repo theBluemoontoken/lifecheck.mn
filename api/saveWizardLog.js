941c95b0f89ac0edbfe4b7f0c4c1dc7c2fa93a15
theBluemoontoken <enhjin.magsarjav@gmail.com>
2025-09-16 19:10:15 +0800

    sda

--- api/saveWizardLog.js
+++ api/saveWizardLog.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
 
     // Google Sheets API Auth
     const auth = new google.auth.JWT(
-      process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      process.env.GOOGLE_CLIENT_EMAIL,
       null,
       (process.env.GOOGLE_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
       ["https://www.googleapis.com/auth/spreadsheets"]
